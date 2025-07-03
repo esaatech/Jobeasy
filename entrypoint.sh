ba9e6ace7995de8aa23e6c71676ddd1a54d3f93e
engrjoel <esaatechnolgy@gmail.com>
2025-07-03 08:10:24 -0400

    amended entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 set -e
 
+# Usual Django setup
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+
 # Populate subscriptions plans
 python manage.py setup_subscription_plans
 
 # Populate home app testimonials and FAQs
 python manage.py bulk_add_testimonials
 python manage.py bulk_add_faq
-
-# Usual Django setup
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
 
 # Start the server with full Gunicorn options
 exec gunicorn jobeas.wsgi:application \
